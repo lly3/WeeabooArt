361a67885ff9cc254d98e2d23723ed6c468f14ea
melon <momobp114@gmail.com>
2022-09-17 14:01:55 +0700

    Update show post routh mock-up

--- client/app/src/views/posts/ShowView.vue
+++ client/app/src/views/posts/ShowView.vue
@@ -3,7 +3,7 @@
     <div class="left-side xl:w-9/12 w-full">
       <div id="default-carousel" class="mx-auto relative overflow-hidden bg-gray-100" data-carousel="static">
         <!-- Carousel wrapper -->
-        <div id="carousel-wrapper" class="h-[70vh] flex" data-slice-index=0>
+        <div id="carousel-wrapper" class="h-[65vh] flex" data-slice-index=0>
           <!-- Item 1 -->
           <div class="duration-700 ease-in-out grow-0 shrink-0 basis-full z-10 my-5">
             <img src="http://localhost/83106977_p0.jpg" class="block h-full object-contain mx-auto">
@@ -24,39 +24,65 @@
             </span>
           </button>  
         </div>
-
       </div>
-      <div class="xl:w-3/6 md:w-4/6 w-5/6  mx-auto">
-        <div class="my-3">
-          <input class="form-control block w-full px-2 py-1 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-lg transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" 
-          type="file"
-          id="browse"
-          name="images" 
-          >
+
+      <div class="p-4 flex border">
+        <div class="flex ml-36">
+          <p>Add to favorites</p>
+        </div>
+      </div>
+
+      <div class="md:w-4/6 w-5/6 mx-auto my-3 space-y-3 relative">
+        <div class="mb-6 space-y-2">
+          <div class="flex items-center">
+            <img src="" class="w-[60px] h-[60px] rounded-xl border" />
+            <div class="flex flex-col ml-3">
+              <p class="text-3xl font-bold">Art Title</p> 
+              <p>Artist Name</p>
+            </div>
+          </div>   
+        </div>
+        <div class="flex space-x-5">
+          <p>Favorites</p>
+          <p>Comments</p>
+          <p>Views</p>
+        </div>
+        <div class="flex space-x-2">
+          <div class="border rounded-lg p-3 text-sm">
+            Tag 1
+          </div>
+          <div class="border rounded-lg p-3 text-sm">
+            Tag 2
+          </div>
+          <div class="border rounded-lg p-3 text-sm">
+            Tag 3
+          </div>
+        </div>
+        <div class="break-all">
+          descriptions aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         </div>
         <div>
-          <div class="mb-6 space-y-2">
-            <label for="default-input" class="block mb-2 text-xl font-medium text-gray-900 dark:text-gray-300">Art Title</label>
-            <input type="text" id="default-input" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <label for="comment" class="block mb-2 text-xl  font-medium text-gray-900 dark:text-gray-300">Description</label>
-            <div class="mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-              <div class="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
-                <textarea id="comment" rows="4" class="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Introduce your art, Tell the backstory, add some intriguing accompanying text, or simply give any extra information you'd like them to know." required></textarea>
-              </div>
-            </div>
-            <label for="default-input" class="block mb-2 text-xl  font-medium text-gray-900 dark:text-gray-300">Add tags</label>
-            <input type="text" id="default-input" placeholder="E.g.: rose, watercolor, painting, fanart, tutorial, photoshop, poetry" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <p class="font-bold">Image details</p>
+          <div>
+            Image size: 
           </div>
-          <div class="flex items-center justify-end py-2 px-3 dark:border-gray-600">
-            <button type="submit" class="inline-flex items-center py-2.5 px-4 text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-            Submit now
-            </button>
+        </div>
+        <div class="mt-6 text-sm">
+          © 2022 TudorPopa
+        </div>
+        <div class="absolute top-0 right-0">
+          Published: Jul 30, 2022
+        </div>
+        <div>
+          <p class="font-bold">Comments</p>
+          <div class="w-full h-[300px] border mt-3">
+            
           </div>
         </div>
       </div>
     </div>
     <div class="right-side p-5 xl:w-3/12 w-full">
-      <p class="my-3">More by _____</p>
+      <p>More by _____</p>
       <div class="w-full h-[250px] mt-3 border">
         
       </div>
