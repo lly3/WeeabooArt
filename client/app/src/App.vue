885aed58e45743e3d580b98ab436249268fcb66c
bluwbyu <ramita.wi@ku.th>
2022-09-29 02:25:18 +0700

    login and logout !!

--- client/app/src/App.vue
+++ client/app/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Profile  from '@/components/profile/Profile.vue'
 </script>
 
 <template>
@@ -14,7 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/rewards">Rewards</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
+        <Profile></Profile>
         <RouterLink to="/register">Register</RouterLink>
       </nav>
     </div>
--- client/app/src/views/LoginView.vue
+++ client/app/src/views/LoginView.vue
@@ -4,35 +4,39 @@
                 <h2 class="text-3xl font-extrabold mb-3">Login</h2>
 
                 <span class="font-bold">Username</span>
-                <div>
-                    <input type="text" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-greenlogo mb-4" placeholder="Username">
-                </div>
-
-                <span class="font-bold">Password</span>
-                <div>
-                    <input type="password" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-greenlogo mb-4" placeholder="Password">
-                </div>
-
-                <div class="block mb-4">
-                    <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-greenlogo shadow-sm focus:border-greenlogo focus:ring focus:ring-secondaryfont focus:ring-opacity-50" name="remember">
-                        <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div class="md:flex md:items-center">
-
-                    <div class="md:w-2/3">
-                        <button class="shadow bg-greenlogo hover:bg-secondaryfont focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-                            Log In
-                        </button>
+                <form @submit.prevent="onFormLogin()">
+                    <div>
+                        <input type="text" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-greenlogo mb-4" placeholder="Username"
+                        v-model="email" required autocomplete="off">
                     </div>
 
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="#">
-                        Forgot your password?
-                    </a>
+                    <span class="font-bold">Password</span>
+                    <div>
+                        <input type="password" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-greenlogo mb-4" placeholder="Password"
+                        v-model="password" required>
+                    </div>
 
-                </div>
+                    <div class="block mb-4">
+                        <label for="remember_me" class="inline-flex items-center">
+                            <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-greenlogo shadow-sm focus:border-greenlogo focus:ring focus:ring-secondaryfont focus:ring-opacity-50" name="remember">
+                            <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                        </label>
+                    </div>
+
+                    <div class="md:flex md:items-center">
+
+                        <div class="md:w-1/3">
+                            <button type="submit" :disabled="disabledButton" class="shadow bg-greenlogo hover:bg-secondaryfont focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+                                Log In
+                            </button>
+                        </div>
+
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="#">
+                            Forgot your password?
+                        </a>
+
+                    </div>
+                </form>
 
             </Login>
         </auth-card>
@@ -40,33 +44,40 @@
 
 <script>
 import AuthCard from '@/components/AuthCard.vue'
+import { useAuthStore } from '@/stores/auth.js'
+
 export default {
     components: {
         AuthCard
     },
+    setup() {
+        const auth_store = useAuthStore()
+        return { auth_store }
+    },
     data() {
         return {
-        username: '',
-        password: '',
-        error: null
+            email: '',
+            password: '',
+            error: null,
+            disabledButton: false
         }
     },
     methods: {
-        async login() {
-        try {
+        async onFormLogin() {
             this.error = null
-            const response = await this.$axios.post('/login', {
-            username: this.username,
-            password: this.password
-            })
-            this.$store.commit('setToken', response.data.token)
-            this.$router.push('/')
-        } catch(error) {
-            console.log(error)
-            this.error = error.message
+            this.disabledButton = true
+            try {
+                if (await this.auth_store.login(this.email, this.password)) {
+                    this.$router.push('/')
+                } else {
+                    this.disabledButton = false
+                }
+            } catch (error) {
+                this.error = error.message
+                this.disabledButton = false
+            }
         }
-        }
-    }
+    },
 }
 </script>
 
