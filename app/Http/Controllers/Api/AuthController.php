ec963bb0cf669e3bf4ce6b0f4a6900a31ae77922
melon <momobp114@gmail.com>
2022-10-18 21:25:25 +0700

    Typo

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -87,7 +87,7 @@ class AuthController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'date_of_birth' => ['required', 'date'],
-            'image_id' => ['integer', 'exists:images,id'],
+            'image_id' => ['integer', 'exists:images_id'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
 
