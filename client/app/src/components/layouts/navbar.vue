55d8a7d445fe9b45806ec318cc75e9a963b81ae9
melon <momobp114@gmail.com>
2022-10-20 02:26:17 +0700

    Dark mode hover

--- client/app/src/components/layouts/navbar.vue
+++ client/app/src/components/layouts/navbar.vue
@@ -41,8 +41,8 @@
           </div>
         </div>
         <div v-else class="flex flex-row space-x-3">
-          <button @click="() => this.$router.push('/register')" class="font-semibold dark:text-white p-2 hover:text-greenlogo duration-200 ease-in">Join</button>
-          <button @click="() => this.$router.push('/login')" class="font-semibold dark:text-white p-2 hover:text-greenlogo duration-200 ease-in">Login</button>
+          <button @click="() => this.$router.push('/register')" class="font-semibold dark:text-white p-2 dark:hover:text-greenlogo hover:text-greenlogo duration-200 ease-in">Join</button>
+          <button @click="() => this.$router.push('/login')" class="font-semibold dark:text-white p-2 dark:hover:text-greenlogo hover:text-greenlogo duration-200 ease-in">Login</button>
         </div>
         <div @click="toggleDarkMode()" class="cursor-pointer bg-gray-200 fill-black p-[10px] rounded-full dark:bg-gray-800 dark:fill-white">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M4.069 13h-4.069v-2h4.069c-.041.328-.069.661-.069 1s.028.672.069 1zm3.034-7.312l-2.881-2.881-1.414 1.414 2.881 2.881c.411-.529.885-1.003 1.414-1.414zm11.209 1.414l2.881-2.881-1.414-1.414-2.881 2.881c.528.411 1.002.886 1.414 1.414zm-6.312-3.102c.339 0 .672.028 1 .069v-4.069h-2v4.069c.328-.041.661-.069 1-.069zm0 16c-.339 0-.672-.028-1-.069v4.069h2v-4.069c-.328.041-.661.069-1 .069zm7.931-9c.041.328.069.661.069 1s-.028.672-.069 1h4.069v-2h-4.069zm-3.033 7.312l2.88 2.88 1.415-1.414-2.88-2.88c-.412.528-.886 1.002-1.415 1.414zm-11.21-1.415l-2.88 2.88 1.414 1.414 2.88-2.88c-.528-.411-1.003-.885-1.414-1.414zm6.312-10.897c-3.314 0-6 2.686-6 6s2.686 6 6 6 6-2.686 6-6-2.686-6-6-6z"/></svg>
