e20516d51d581d483dcd9b7aa38f707b5dbc06e4
melon <momobp114@gmail.com>
2022-10-20 15:27:36 +0700

    Change comments box color

--- client/app/src/views/posts/ShowView.vue
+++ client/app/src/views/posts/ShowView.vue
@@ -114,10 +114,10 @@
                 <img src="" alt="" /> 
               </div>
               <div class="w-full rounded-lg text-center bg-gray-100 dark:bg-gray-700 font-bold text-gray-500 dark:text-gray-300">
-                <div class="w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-                  <div class="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
+                <div class="w-full bg-gray-200 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+                  <div class="py-2 px-4 bg-gray-100 rounded-t-lg dark:bg-gray-800">
                     <label for="comment" class="sr-only">Your comment</label>
-                    <textarea id="comment" rows="4" class="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required></textarea>
+                    <textarea id="comment" rows="4" class="px-0 bg-gray-100 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required></textarea>
                   </div>
                   <div class="flex justify-between items-center py-2 px-3 border-t dark:border-gray-600">
                     <button type="submit" class="inline-flex ml-auto items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
