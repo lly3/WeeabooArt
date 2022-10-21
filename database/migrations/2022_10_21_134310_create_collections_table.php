5f1f4f946d8d6dada0143d14fc8bc43c5b733646
melon <momobp114@gmail.com>
2022-10-21 13:58:53 +0700

    Change column name

--- database/migrations/2022_10_21_134310_create_collections_table.php
+++ database/migrations/2022_10_21_134310_create_collections_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +16,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('collections', function (Blueprint $table) {
-            $table->id();
+            $table->id('collection_id');
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Post::class);
             $table->timestamps();
         });
     }
