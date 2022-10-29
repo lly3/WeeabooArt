5c1599ba31047dcc1fcb8df30a9788876cc9fa53
ssunny-mengg <khemmaporn.n@ku.th>
2022-10-30 02:02:45 +0700

    Update favorite count css

--- client/app/src/components/PostCard.vue
+++ client/app/src/components/PostCard.vue
@@ -22,16 +22,10 @@
                 <figcaption>
                     <h3>{{ post.title }}</h3>
                     <h5>By {{ post.user_name}}</h5>
+                    <h3>{{ post.favorite_count }} Like</h3>
                 </figcaption>
             </figure>
             <a href=""></a>
-            <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                     stroke="currentColor">
-                    <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                </svg>
-                <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ post.favorite_count}}</span>
-            </button>
         </a>
     </div>
 
--- client/app/src/views/GalleryView.vue
+++ client/app/src/views/GalleryView.vue
@@ -27,16 +27,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostLiked[0].title }}</h3>
                                 <h5>By {{ posts_mostLiked[0].user_name}}</h5>
+                                <h3>{{ posts_mostLiked[0].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                       <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostLiked[0].favorite_count}}</span>
-                        </button>
                     </a>
                     <a :href="'http://localhost:3000/post/' + posts_mostLiked[1].id" class="p-1 md:p-2 w-1/2 button-container" v-if="have2Posts">
                         <figure class="snip">
@@ -45,16 +39,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostLiked[1].title }}</h3>
                                 <h5>By {{ posts_mostLiked[1].user_name}}</h5>
+                                <h3>{{ posts_mostLiked[1].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostLiked[1].favorite_count}}</span>
-                        </button>
                     </a>
                     <a :href="'http://localhost:3000/post/' + posts_mostLiked[2].id" class="w-full p-1 md:p-2 button-container" v-if="have3Posts">
                         <figure class="snip">
@@ -63,16 +51,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostLiked[2].title }}</h3>
                                 <h5>By {{ posts_mostLiked[2].user_name}}</h5>
+                                <h3>{{ posts_mostLiked[2].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostLiked[2].favorite_count}}</span>
-                        </button>
                     </a>
                 </div>
                 <div class="flex flex-wrap w-1/2" v-if="mostLikes">
@@ -83,16 +65,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostLiked[3].title }}</h3>
                                 <h5>By {{ posts_mostLiked[3].user_name}}</h5>
+                                <h3>{{ posts_mostLiked[3].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostLiked[3].favorite_count}}</span>
-                        </button>
                     </a>
                     <a :href="'http://localhost:3000/post/' + posts_mostLiked[4].id" class="w-1/2 p-1 md:p-2 button-container" v-if="have5Posts">
                         <figure class="snip">
@@ -101,16 +77,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostLiked[4].title }}</h3>
                                 <h5>By {{ posts_mostLiked[4].user_name}}</h5>
+                                <h3>{{ posts_mostLiked[4].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostLiked[4].favorite_count}}</span>
-                        </button>
                     </a>
                     <a :href="'http://localhost:3000/post/' + posts_mostLiked[5].id" class="w-1/2 p-1 md:p-2 button-container" v-if="have6Posts">
                         <figure class="snip">
@@ -119,16 +89,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostLiked[5].title }}</h3>
                                 <h5>By {{ posts_mostLiked[5].user_name}}</h5>
+                                <h3>{{ posts_mostLiked[5].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostLiked[5].favorite_count}}</span>
-                        </button>
                     </a>
                 </div>
             </div>
@@ -146,16 +110,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostViewed[0].title }}</h3>
                                 <h5>By {{ posts_mostViewed[0].user_name}}</h5>
+                                <h3>{{ posts_mostViewed[0].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostViewed[0].favorite_count}}</span>
-                        </button>
                     </a>
                     <a :href="'http://localhost:3000/post/' + posts_mostViewed[1].id" class="p-1 md:p-2 w-1/2 button-container" v-if="have2Posts">
                         <figure class="snip">
@@ -164,16 +122,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostViewed[1].title }}</h3>
                                 <h5>By {{ posts_mostViewed[1].user_name}}</h5>
+                                <h3>{{ posts_mostViewed[1].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostViewed[1].favorite_count}}</span>
-                        </button>
                     </a>
                     <a :href="'http://localhost:3000/post/' + posts_mostViewed[2].id" class="w-full p-1 md:p-2 button-container" v-if="have3Posts">
                         <figure class="snip">
@@ -182,16 +134,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostViewed[2].title }}</h3>
                                 <h5>By {{ posts_mostViewed[2].user_name}}</h5>
+                                <h3>{{ posts_mostViewed[2].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostViewed[2].favorite_count}}</span>
-                        </button>
                     </a>
                 </div>
                 <div class="flex flex-wrap w-1/2" v-if="mostViews">
@@ -202,16 +148,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostViewed[3].title }}</h3>
                                 <h5>By {{ posts_mostViewed[3].user_name}}</h5>
+                                <h3>{{ posts_mostViewed[3].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostViewed[3].favorite_count}}</span>
-                        </button>
                     </a>
                     <a :href="'http://localhost:3000/post/' + posts_mostViewed[4].id" class="w-1/2 p-1 md:p-2 button-container" v-if="have5Posts">
                         <figure class="snip">
@@ -220,16 +160,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostViewed[4].title }}</h3>
                                 <h5>By {{ posts_mostViewed[4].user_name}}</h5>
+                                <h3>{{ posts_mostViewed[4].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostViewed[4].favorite_count}}</span>
-                        </button>
                     </a>
                     <a :href="'http://localhost:3000/post/' + posts_mostViewed[5].id" class="w-1/2 p-1 md:p-2 button-container" v-if="have6Posts">
                         <figure class="snip">
@@ -238,16 +172,10 @@
                             <figcaption>
                                 <h3>{{ posts_mostViewed[5].title }}</h3>
                                 <h5>By {{ posts_mostViewed[5].user_name}}</h5>
+                                <h3>{{ posts_mostViewed[5].favorite_count }} Like</h3>
                             </figcaption>
                         </figure>
                         <a href=""></a>
-                        <button class="m-4 btn-glow h-6 px-4 sm:h-8 sm:px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 13 13"
-                                 stroke="currentColor">
-                                <path d="M6.99999 11.8709L6.15416 11.1009C3.14999 8.37675 1.16666 6.57425 1.16666 4.37508C1.16666 2.57258 2.57832 1.16675 4.37499 1.16675C5.38999 1.16675 6.36416 1.63925 6.99999 2.38008C7.63582 1.63925 8.60999 1.16675 9.62499 1.16675C11.4217 1.16675 12.8333 2.57258 12.8333 4.37508C12.8333 6.57425 10.85 8.37675 7.84582 11.1009L6.99999 11.8709Z" fill="#F85486"></path>
-                            </svg>
-                            <span class="text-black text-[12px] pl-1 font-semibold hidden sm:block">{{ posts_mostViewed[5].favorite_count}}</span>
-                        </button>
                     </a>
                 </div>
             </div>
