fffaa6ab14f0749cef345a5e3fab8df2a4120a47
ssunny-mengg <khemmaporn.n@ku.th>
2022-10-30 00:42:51 +0700

    Update GalleryView.vue

--- client/app/src/views/GalleryView.vue
+++ client/app/src/views/GalleryView.vue
@@ -255,56 +255,27 @@ export default {
             return this.posts_mostViewed.length
         },
         havePosts() {
-            if (this.posts_mostViewed.length > 0) {
-                return true
-            } else {
-                return false
-            }
+            return this.posts_mostViewed.length > 0;
         },
         have1Post() {
-            if (this.posts_mostViewed.length >= 1) {
-                return true
-            } else {
-                return false
-            }
+            return this.posts_mostViewed.length >= 1;
         },
         have2Posts() {
-            if (this.posts_mostViewed.length >= 2) {
-                return true
-            } else {
-                return false
-            }
+            return this.posts_mostViewed.length >= 2;
         },
         have3Posts() {
-            if (this.posts_mostViewed.length >= 3) {
-                return true
-            } else {
-                return false
-            }
+            return this.posts_mostViewed.length >= 3;
         },
         have4Posts() {
-            if (this.posts_mostViewed.length >= 4) {
-                return true
-            } else {
-                return false
-            }
+            return this.posts_mostViewed.length >= 4;
         },
         have5Posts() {
-            if (this.posts_mostViewed.length >= 5) {
-                return true
-            } else {
-                return false
-            }
+            return this.posts_mostViewed.length >= 5;
         },
         have6Posts() {
-            if (this.posts_mostViewed.length >= 6) {
-                return true
-            } else {
-                return false
-            }
+            return this.posts_mostViewed.length >= 6;
         },
     },
-
     async mounted() {
         try {
             this.error = null
@@ -396,5 +367,108 @@ export default {
     justify-content: center;
     text-align: center;
     padding: 10px;
+}
+
+.snip {
+    position: relative;
+    display: inline-block;
+    overflow: hidden;
+    margin: 10px 8px;
+    width: 100%;
+    color: #ffffff;
+    text-align: center;
+}
+
+.snip * {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+}
+
+.snip:before {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    right: 10px;
+    bottom: 10px;
+    top: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+    content: '';
+    -webkit-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+    -webkit-transition-delay: 0.3s;
+    transition-delay: 0.3s;
+}
+
+.snip img {
+    max-width: 100%;
+    backface-visibility: hidden;
+    vertical-align: top;
+}
+
+.snip figcaption {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.snip h3,
+.snip h5 {
+    margin: 0;
+    opacity: 0;
+    letter-spacing: 1px;
+}
+
+.snip h3 {
+    -webkit-transform : translateY(-100%);
+    transform : translateY(-100%);
+    text-transform: uppercase;
+    font-weight: 400;
+    -webkit-transition-delay: 0.05s;
+    transition-delay: 0.05s;
+    margin-bottom: 5px;
+}
+
+.snip h5 {
+    font-weight: 400;
+    background-color: #A0A0A0;
+    padding: 3px 10px;
+    -webkit-transform : translateY(100%);
+    transform : translateY(100%);
+    -webkit-transition-delay: 0s;
+    transition-delay: 0s;
+}
+
+.snip a {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+}
+
+.snip:hover:before,
+.snip.hover:before {
+    top: 10px;
+    -webkit-transition-delay: 0s;
+    transition-delay: 0s;
+}
+
+.snip:hover h3,
+.snip.hover h3,
+.snip:hover h5,
+.snip.hover h5 {
+    opacity: 1;
+    -webkit-transform : translateY(0);
+    transform : translateY(0);
 }
 </style>
