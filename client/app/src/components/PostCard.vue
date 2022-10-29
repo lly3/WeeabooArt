3cd87b373b0a8930bc19f4803bb3b254b30366a0
ssunny-mengg <khemmaporn.n@ku.th>
2022-10-29 23:18:47 +0700

    Update PostCard.vue css

--- client/app/src/components/PostCard.vue
+++ client/app/src/components/PostCard.vue
@@ -122,4 +122,108 @@ export default {
         object-position: center;
     }
 }
+.snip {
+    position: relative;
+    display: inline-block;
+    overflow: hidden;
+    margin: 10px 8px;
+    min-width: 230px;
+    max-width: 315px;
+    width: 100%;
+    color: #ffffff;
+    text-align: center;
+}
+
+.snip * {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+}
+
+.snip:before {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    right: 10px;
+    bottom: 10px;
+    top: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+    content: '';
+    -webkit-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+    -webkit-transition-delay: 0.3s;
+    transition-delay: 0.3s;
+}
+
+.snip img {
+    max-width: 100%;
+    backface-visibility: hidden;
+    vertical-align: top;
+}
+
+.snip figcaption {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.snip h3,
+.snip h5 {
+    margin: 0;
+    opacity: 0;
+    letter-spacing: 1px;
+}
+
+.snip h3 {
+    -webkit-transform : translateY(-100%);
+    transform : translateY(-100%);
+    text-transform: uppercase;
+    font-weight: 400;
+    -webkit-transition-delay: 0.05s;
+    transition-delay: 0.05s;
+    margin-bottom: 5px;
+}
+
+.snip h5 {
+    font-weight: 400;
+    background-color: #A0A0A0;
+    padding: 3px 10px;
+    -webkit-transform : translateY(100%);
+    transform : translateY(100%);
+    -webkit-transition-delay: 0s;
+    transition-delay: 0s;
+}
+
+.snip a {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+}
+
+.snip:hover:before,
+.snip.hover:before {
+    top: 10px;
+    -webkit-transition-delay: 0s;
+    transition-delay: 0s;
+}
+
+.snip:hover h3,
+.snip.hover h3,
+.snip:hover h5,
+.snip.hover h5 {
+    opacity: 1;
+    -webkit-transform : translateY(0);
+    transform : translateY(0);
+}
 </style>
