e11f716b1eef96b8ff3dec87b616fe1195a5fea7
kritsada-kr <78583508+kritsada-kr@users.noreply.github.com>
2022-10-30 12:19:31 +0700

    merged with main to branch

--- client/app/src/views/tags/AllTagView.vue
+++ client/app/src/views/tags/AllTagView.vue
@@ -1,19 +1,29 @@
-<template>
-    <div class="mx-8">
-        <div class="mx-8">
-            <h1 class="text-3xl text-yellow-700">Tags</h1>
+<template >
+    <body>
+        <div class="mx-8 ">
+            <div class="mx-8">
+                <h1 class="text-3xl text-yellow-700">Tags</h1>
 
-            <div v-for="tag in tags" :key="tag.id"
-                 :tag="{...tag}"  >
+                <div class="flex flex-wrap justify-start gap-4 space-x-2 text-lg">
+                    <div v-for="tag in tags" class="flex inline-flex items-center gap-[5px] border border-gray-300 dark:border-gray-600 dark:bg-gray-800 cursor-pointer rounded-md p-3 dark:text-white"  @click="() => this.$router.push(`/tags/${tag.id}`)">
+                        {{ tag.name }}
+                    </div>
 
-                <button @click="selectTag(tag)"
-                        class="p-2 bg-blue-200 border-2 border-blue-400 rounded-xl">
-                    {{ tag.name }}
-                </button>
+                </div>
+
+<!--                <div v-for="tag in tags" :key="tag.id"-->
+<!--                     :tag="{...tag}"  >-->
+
+<!--                    <button @click="selectTag(tag)"-->
+<!--                            class="p-2 bg-blue-200 border-2 border-blue-400 rounded-xl">-->
+<!--                        {{ tag.name }}-->
+<!--                    </button>-->
+<!--                </div>-->
+
             </div>
-
         </div>
-    </div>
+    </body>
+
 </template>
 
 <script>
