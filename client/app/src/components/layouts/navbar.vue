94c82fa458f5e2db6d6170325ea431d894d434e5
bluwbyu <ramita.wi@ku.th>
2022-11-02 23:07:17 +0700

    Update navbar.vue

--- client/app/src/components/layouts/navbar.vue
+++ client/app/src/components/layouts/navbar.vue
@@ -22,7 +22,7 @@
         <div>
           <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-lg md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
             <span class="sr-only">Open user menu</span>
-            <img class="w-8 h-8 rounded-md object-cover" :src=imageURL(this.auth_store.getImage)>
+            <img v-if="auth_store.isAuthen" class="w-8 h-8 rounded-md object-cover" :src=imageURL(this.auth_store.getImage)>
           </button>
 
           <!-- Dropdown menu -->
