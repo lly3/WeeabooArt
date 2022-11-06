4a2cc706a98d7e2ecf529157a7c1330fb5ad79e4
melon <momobp114@gmail.com>
2022-11-06 11:55:39 +0700

    Add image on TagCard

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -17,6 +17,7 @@ class TagResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'image' => $this->posts[0]->image->path
         ];
     }
 }
--- client/app/src/services/api.js
+++ client/app/src/services/api.js
@@ -150,6 +150,9 @@ export const tagAPI = {
     paginate(tag, pageNumber, quantity=0) {
         return axiosInstance.get(`/tags/${tag}?page=${pageNumber}&quantity=${quantity}`);
     },
+    fetch() {
+        return axiosInstance.get('/tags')
+    }
 }
 
 export const commentAPI = {
